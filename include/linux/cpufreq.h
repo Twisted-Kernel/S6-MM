aeb12eacb49bf4d7d07d64ddf898ec82b05b2aae
javilonas <admin@lonasdigital.com>
2015-07-27 01:52:47 -0400

    cpufreq: Add new governor Barry-Allen [@Javilonas]

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -427,6 +427,9 @@ extern struct cpufreq_governor cpufreq_gov_interextrem;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
 extern struct cpufreq_governor cpufreq_gov_cafactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
+extern struct cpufreq_governor cpufreq_gov_barry_allen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
 #endif
 
 
